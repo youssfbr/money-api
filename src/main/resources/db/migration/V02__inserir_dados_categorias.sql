a499967f94f562611d08bfd5c227e1a89a72b832
Alisson Youssf <youssfbr@gmail.com>
2020-01-02 12:07:23 -0300

    migrando scripts

---
+++ src/main/resources/db/migration/V02__inserir_dados_categorias.sql
@@ -0,0 +1,7 @@
+INSERT INTO categoria (nome) VALUES ('Alimentação');
+INSERT INTO categoria (nome) VALUES ('Recarga Celular');
+INSERT INTO categoria (nome) VALUES ('Almoço');
+INSERT INTO categoria (nome) VALUES ('Curso online');
+INSERT INTO categoria (nome) VALUES ('Energia');
+INSERT INTO categoria (nome) VALUES ('Internet');
+INSERT INTO categoria (nome) VALUES ('Ração Pets');
